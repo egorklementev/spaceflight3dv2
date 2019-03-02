8fad767c4161920fcc46ba46266f71d21a29f2c4
Egor Klementev <egorklementeff@yandex.ru>
2019-03-02 22:55:04 +0300

    Fade & loading screen fixed b0203191

---
+++ General/Loading.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour {
+
+    public static int levelToLoad = 1;
+
+    public Slider loadingBar;
+
+    private void Awake()
+    {
+        StartCoroutine(LoadAsynchronously(levelToLoad));
+    }
+
+    private IEnumerator LoadAsynchronously(int levelToLoad)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelToLoad);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            Debug.Log("Loading: " + progress);
+            loadingBar.value = progress;
+
+            yield return null;
+        }
+
+    }
+
+
+
+}
