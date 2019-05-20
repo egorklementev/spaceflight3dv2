f182e855acb21e809794d1d6e4bbdec867ed0c1f
Egor Klementev <egorklementeff@yandex.ru>
2019-05-20 23:25:00 +0300

    Editor interface optimizations

---
+++ Game/UI/EditorUIUnit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class EditorUIUnit : MonoBehaviour {
+
+    public TextMeshProUGUI fpsText;
+    public TextMeshProUGUI xSizeText;
+    public TextMeshProUGUI ySizeText;
+    public TextMeshProUGUI availableColorsText;
+    public TextMeshProUGUI sequenceSizeText;
+
+    public EditorParams pu;
+
+	void Update () {
+
+        fpsText.text = "FPS: " + (1f / Time.deltaTime).ToString("0");
+        xSizeText.text = "Grid width: " + pu.gridSize.x.ToString("0");
+        ySizeText.text = "Grid heigth: " + pu.gridSize.y.ToString("0");
+        availableColorsText.text = "Colors number: " + pu.colorsAvailable.ToString();
+        sequenceSizeText.text = "Sequence size: " + pu.sequenceSize.ToString();
+
+    }
+}
---
+++ GameEditor/EditorInput.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class EditorInput : MonoBehaviour
+{
+
+    [Header("Units' refs")]
+    public EditorLogic lu;
+    public EditorGraphics gu;
+    public EditorParams pu;
+
+    [HideInInspector]
+    public bool wasSwap = false;
+
+    private bool isGameOver = false;
+
+    private void Start() { }
+
+    private void Update()
+    {   
+
+        // TODO: implement input feature
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void SetGameOver()
+    {
+        isGameOver = true;
+    }
+
+}
